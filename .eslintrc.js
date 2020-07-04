510f21441b7f998838c4cda3d7f054e487b708d6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-04 10:10:38 -0300

    Updates eslint with import settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,13 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ['src'],
+        extensions: ['.jsx', '.js']
+      }
+    }
   }
 }
