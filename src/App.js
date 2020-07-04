392a7a249dbe363d6a76683bec9056319e039c68
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-07-03 22:26:46 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Routes from 'routes'
 
+import GlobalStyles from './styles/global'
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyles />
+    </>
   )
 }
 
